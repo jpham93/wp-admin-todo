7c6a6feafe01cc67169500889b8a30f1baa70492
James Pham <jamespham93@yahoo.com>
2021-03-05 08:01:55 -0800

    WIP - create new list page

---
+++ admin/partials/wp-admin-todo-admin-create-list-display.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Create New TODO List view
+*
+* @link       https://jamespham.io
+* @since      2.0.0
+*
+* @package    Wp_Admin_Todo
+* @subpackage Wp_Admin_Todo/admin/partials
+*/
+?>
+<h1>Create a New List</h1>
+
+<label for="todo-new-list">New List Name</label>
+<input type="text" id="todo-new-list-input">
+<button id="todo-new-list-create" class="btn btn-outline-success">Create</button>
+
+<script>
+    const ajaxUrl = '<?php echo admin_url('admin-ajax.php') ?>
+
+    // CREATE LIST
+    
+</script>
